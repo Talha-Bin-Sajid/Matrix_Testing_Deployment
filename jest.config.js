3fce8a213555e765d86e69303242c658d8980665
Talha bin sajid <talhabinsajid04@gmail.com>
2025-09-18 23:21:49 +0500

    Fix workflow issues and config files

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,18 @@
+// jest.config.js
 module.exports = {
   testEnvironment: 'node',
   collectCoverage: true,
   coverageDirectory: 'coverage',
   coverageReporters: ['text', 'lcov'],
+  testMatch: [
+    '**/*.test.js',
+    '**/__tests__/**/*.js'
+  ],
+  reporters: [
+    'default',
+    ['jest-junit', {
+      outputDirectory: 'test-results',
+      outputName: 'junit.xml',
+    }]
+  ]
 };
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,12 @@
+// webpack.config.js
 const path = require('path');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: './index.js',  // Updated to match your file structure
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
+    clean: true,
   },
+  mode: 'production',
 };
